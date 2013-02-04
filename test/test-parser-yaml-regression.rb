4c47f2a16ec0b2a6e771f126a76defe095d35a29
Pascal-Louis Perez <pascal@squareup.com>
2013-02-03 17:55:37 -0800

    More detailed tests added to regression. (These duplicate some in the main parser-yaml test.)

--- test/test-parser-yaml-regression.rb
+++ test/test-parser-yaml-regression.rb
@@ -21,8 +21,44 @@ foo:
     - baz_deep: some_text
 YAML
 
+    parser = Kwalify::Yaml::Parser.new
     assert_equal(
       {"foo"=>{"bar"=>{"baz"=>[{"baz_deep"=>"some_text"}]}}},
+      parser.parse(content)
+      )
+    assert_equal([3, 7], parser.location("/foo/bar/baz"))
+    assert_equal([4, 7], parser.location("/foo/bar/baz/baz_deep"))
+  end
+
+  def test_sequence3
+    content = <<-YAML
+- A
+-
+- C
+-
+-
+-
+- G
+    YAML
+
+    assert_equal(
+      ["A", nil, "C", nil, nil, nil, "G"],
+      Kwalify::Yaml::Parser.new.parse(content)
+      )
+  end
+
+  def test_sequence4
+    content = <<-YAML
+-
+  -
+    -
+    -
+    -
+-
+YAML
+
+    assert_equal(
+      [[[nil, nil, nil]], nil],
       Kwalify::Yaml::Parser.new.parse(content)
       )
   end
